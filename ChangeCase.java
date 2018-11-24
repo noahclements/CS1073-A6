728f49a4c0caeb952784aafb233650cd74370c24
Noah Clements <noahnoah900@gmail.com>
2018-11-23 21:44:59 -0400

    initial commit for ChangeCase

---
+++ ChangeCase.java
@@ -0,0 +1,30 @@
+public class ChangeCase {
+	private String input
+
+	public ChangeCase(String inputIn) {
+		input = inputIn;
+	}
+
+		public String toLowerCase() {
+			return input.toLowerCase();
+		}
+
+		public String toUpperCase() {
+			return input.toUpperCase();
+		}
+
+		public String toggleCase() {
+
+		}
+
+		public String toTitleCase() {
+
+		}
+
+		public String toSentenseCase() {
+
+		}
+
+
+
+}

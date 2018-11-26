2b428d61b7a58bc435128c7dd61a73855300a3c7
Noah Clements <noahnoah900@gmail.com>
2018-11-26 18:37:05 -0400

    graph functions correctly

--- IceCreamScatterPlot.java
+++ IceCreamScatterPlot.java
@@ -29,29 +29,31 @@ public class IceCreamScatterPlot {
 			int dollarPrint = 10;
 			int cashPrice = 0;
 			System.out.println(title);
-			for(int row = 0; row < 11; row++) {
+			for(int row = 0; row < 12; row++) {
 				if(dollarPrint == 0) {
 					cashPrice = 0;
 					System.out.print("$"+(cashPrice) + "\t" + "L");	// change that later
-				} else {
+				} else if(row == 11){
+					System.out.print("\t");
+				}else {
 					cashPrice = dollarPrint * 100;
 					System.out.print("$"+(cashPrice) + "\t" + "|");
 				}
-				dollarPrint--;
+				//dollarPrint--;
 					for(int column = 0; column < valuesArray.length; column++) {
 						if(valuesArray[column] == cashPrice && valuesArray[column] != 0) {
 							System.out.print("*");
 						} else if(dollarPrint == 0) {
 							System.out.print("-");
 						} else if(dollarPrint == -1 && columnNumPrint < 31) {
-							System.out.print(columnNumPrint + "     ");
+							System.out.print(columnNumPrint + "    ");
 							columnNumPrint += 5;
 						} else {
 							System.out.print(" ");
 						}
 					}
 					System.out.println();
-					//dollarPrint--;
+					dollarPrint--;
 				}
 					
 			

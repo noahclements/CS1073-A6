f84416a64f37bbefac24cc1c183b397125532293
Noah Clements <noahnoah900@gmail.com>
2018-11-25 21:16:56 -0400

    arrays are done correctly, prints out fine

--- IceCreamScatterPlot.java
+++ IceCreamScatterPlot.java
@@ -4,31 +4,64 @@ public class IceCreamScatterPlot {
 		int temp = 0;
 		int price = 0;
 		int counter = 0;
-		int[][]valuesArray;
+		int[][] valuesArray;
 
-		//Scanner sc = new Scanner(System.in);
-		//String file = sc.nextLine();
-		//String temp1 = sc.nextLine();
 		Scanner tokenizer = new Scanner(System.in);
 		
 		
 		tokenizer.useDelimiter(",|\\s");
 		String title = tokenizer.nextLine();
-		//int temp1 = tokenizer.nextInt();
-		//tokenizer.next();
+		//String numsRepeat = "";
+		String priceRepeat = "";
+		String tempRepeat = "";
 		while(tokenizer.hasNextInt()) {
-
+			
 			temp = tokenizer.nextInt();
 			price = tokenizer.nextInt();
+
+			tempRepeat += temp + "\n";
+			priceRepeat += price + "\n";
+
+			//numsRepeat += (temp + "," + price + "\n");
+
 			tokenizer.next();
 			
+
 			System.out.println(temp);
 			System.out.println(price);
 			counter++;
 		}
+		valuesArray = new int[2][counter];
+
+		//tokenizer = new Scanner(numsRepeat);
+		Scanner tempScan = new Scanner(tempRepeat);
+		Scanner priceScan = new Scanner(priceRepeat);
+		tempScan.useDelimiter("\n");
+		priceScan.useDelimiter("\n");
+		
+			
+		
+			for(int x = 0; x < valuesArray[0].length; x++) {
+				valuesArray[0][x] = tempScan.nextInt();
+				valuesArray[1][x] = priceScan.nextInt(); 
+			}
+		
+			
+
+			// PRINTING 2D Array
+			for(int i = 0; i < valuesArray.length; i++){
+				for(int j = 0; j < valuesArray[0].length; j++){
+					System.out.print(valuesArray[i][j] + " ");
+				}
+				System.out.println();
+			}
+		
+
 		System.out.println("TITLE IS" + title);
-		//System.out.println("temp: " + temp1);
+		System.out.println("array length is : " + valuesArray[0].length);
 		System.out.println("array should be " + counter);
+		//System.out.println(valuesArray[1][7]);
+
 
 
 	}

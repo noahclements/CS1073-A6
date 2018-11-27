85a4515967140b59b6a8ee702398cdd95f4e921e
Noah Clements <noahnoah900@gmail.com>
2018-11-27 16:35:27 -0400

    upper, lower, and toggleCase finished

--- ChangeCase.java
+++ ChangeCase.java
@@ -1,39 +1,67 @@
 import java.util.Scanner;
 public class ChangeCase {
-	private String input
-	private String temp;
+	private String input;
+	private char temp;
+	private int counter = 0;
+	private String test = "";
+	private String newSentence = "";
 
 	public ChangeCase(String inputIn) {
 		input = inputIn;
 	}
 
 		public String toLowerCase() {
-			return input.toLowerCase();
+			for(int i = 0; i < input.length(); i++) {
+				temp = input.charAt(i);
+				if(temp >= 'A' && temp <= 'Z') {
+					temp += 32;
+					newSentence += temp;
+				} else {
+					newSentence += temp;
+				}
+			}
+			return newSentence;
 		}
 
 		public String toUpperCase() {
-			return input.toUpperCase();
+			for(int i = 0; i < input.length(); i++) {
+				temp = input.charAt(i);
+				if(temp >= 'a' && temp <= 'z') {
+					temp -= 32;
+					newSentence += temp;
+				} else {
+					newSentence += temp;
+				}
+			}
+			return newSentence;
 		}
 
 		public String toggleCase() {
-			for(int i = 0; i < input.length; i++) {
-				if(input.charAt(i).isUpperCase()) {
-					input.charAt(i) = input.charAt(i).toLowerCase();
-				} else if(input.charAt(i).isLowerCase()) {
-					input.charAt(i) = input.charAt(i).toUpperCase();
+			
+			for(int i = 0; i < input.length(); i++) {
+				temp = input.charAt(i);
+				if(temp >= 'a' && temp <= 'z') {
+					temp -= 32;
+					newSentence += temp;
+				} else if(temp >= 'A' && temp <= 'Z') {
+					temp += 32;
+					newSentence += temp;
 				}
 			}
-			return input;
+			return newSentence;
 		}
 
 		public String toTitleCase() {
-			Scanner lineScan = new Scanner(input);
+		//	Scanner lineScan = new Scanner(input);
+			return " ";
 			
 		}
 
 		public String toSentenseCase() {
-			return input.toUpperCase(input.charAt(0));
+			//return input.toUpperCase(input.charAt(0));
+			return " ";
 		}
+
 
 
 
--- ChangeCaseDriver.java
+++ ChangeCaseDriver.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 public class ChangeCaseDriver {
 	public static void main(String[] args) {
-		String input = "";
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter a string.");
 
-		input = sc.nextLine();
+		String input = sc.nextLine();
+		String input2 = input;
+		String input3 = input;
+		ChangeCase p1 = new ChangeCase(input);
+		ChangeCase p2 = new ChangeCase(input2);
+		ChangeCase p3 = new ChangeCase(input3);
+		System.out.println(p1.toLowerCase());
+		System.out.println(p2.toUpperCase());
+		System.out.println(p3.toggleCase());
 
 		
 

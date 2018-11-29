0dce43b12699e7f7d5d119ac21458c09dabf5c53
Noah Clements <noahnoah900@gmail.com>
2018-11-29 12:34:19 -0400

    javadoc comments, and changed a variable

--- ChangeCase.java
+++ ChangeCase.java
@@ -1,96 +1,119 @@
+/**
+	A class representing the many methods to changing cases
+	@author Noah Clements 3585596
+*/
 import java.util.Scanner;
 public class ChangeCase {
+	/** the user input */
 	private String input;
-	private char temp;
-	private int counter = 0;
-	private char test;
+	/** char value for the user input */
+	private char ch;
+	/** the sentence that will be returned */
 	private String newSentence = "";
+	/** the boolean value to say whether its the end of a sentence */
 	private boolean endOfSentence = false;
-
+	/**
+	The constructor method for ChangeCase
+	@param inputIn the user input
+	*/
 	public ChangeCase(String inputIn) {
 		input = inputIn;
 	}
-
+		/**
+			converts the cases to all lower case
+			@return the string with all lower case letters
+		*/
 		public String toLowerCase() {
 			for(int i = 0; i < input.length(); i++) {
-				temp = input.charAt(i);
-				if(temp >= 'A' && temp <= 'Z') {
-					temp += 32;
+				ch = input.charAt(i);
+				if(ch >= 'A' && ch <= 'Z') {
+					ch += 32;
 				} 
-				newSentence += temp;
+				newSentence += ch;
 			}
 			return newSentence;
 		}
-
+		/**
+			converts the words to all upper case
+			@return the string with all upper case words
+		*/
 		public String toUpperCase() {
 			for(int i = 0; i < input.length(); i++) {
-				temp = input.charAt(i);
-				if(temp >= 'a' && temp <= 'z') {
-					temp -= 32;
+				ch = input.charAt(i);
+				if(ch >= 'a' && ch <= 'z') {
+					ch -= 32;
 				}
-				newSentence += temp;
+				newSentence += ch;
 			}
 			return newSentence;
 		}
-
+		/**
+			converts it so that every word starts with a capital letter, the rest lower case
+			@return the string with every word starting with a capital letter, all other letters lowercase
+		*/
 		public String toTitleCase() {
-
-	for(int i = 0; i < input.length(); i++) {
-				temp = input.charAt(i);
+			for(int i = 0; i < input.length(); i++) {
+				ch = input.charAt(i);
 				if(i == 0) {
-					if(temp >= 'a' && temp <= 'z') {
-						temp -= 32;
+					if(ch >= 'a' && ch <= 'z') {
+						ch -= 32;
 					} 
-				} else if(temp == ' ') {
+				} else if(ch == ' ') {
 					i++;
-					temp = input.charAt(i);
+					ch = input.charAt(i);
 					if(i< input.length()) {
-						if(temp >= 'a' && temp <= 'z') {
-							temp -= 32;
+						if(ch >= 'a' && ch <= 'z') {
+							ch -= 32;
 						} 
 					}
 					newSentence += " ";
-				} else if(temp >= 'A' && temp <= 'Z') {
-					temp += 32;
+				} else if(ch >= 'A' && ch <= 'Z') {
+					ch += 32;
 				} 
-				newSentence += temp;
+				newSentence += ch;
 			}			
 		return newSentence;
 	}
 
-
+		/**
+			converts all lowercase to upper case, and vice versa
+			@return the string with all lowercase letters to upper, and vice versa
+		*/
 		public String toggleCase() {
 		for(int i = 0; i < input.length(); i++) {
-				temp = input.charAt(i);
-				if(temp >= 'a' && temp <= 'z') {
-					temp -= 32;
-				} else if(temp >= 'A' && temp <= 'Z') {
-					temp += 32;
+				ch = input.charAt(i);
+				if(ch >= 'a' && ch <= 'z') {
+					ch -= 32;
+				} else if(ch >= 'A' && ch <= 'Z') {
+					ch += 32;
 				}
-				newSentence += temp;
+				newSentence += ch;
 			}
 			return newSentence;
 			
 		}
-
+		/**
+			makes sure the first word in any sentence starts with a capital letter
+			@return the string with the first word in the sentence starting with a capital letter, the rest lower case
+		*/
 		public String toSentenceCase() {
 		 for(int i = 0; i < input.length(); i++) {
-		 	temp = input.charAt(i);
-		 	if(temp == '!' || temp == '?' || temp == '.') {
+		 	ch = input.charAt(i);
+		 	if(ch == '!' || ch == '?' || ch == '.') {
 		 		endOfSentence = true;
 		 	}
 
 		 	if(i == 0 || endOfSentence == true) {
-		 		if(temp >= 'A' && temp <= 'Z') {
+		 		if(ch >= 'A' && ch <= 'Z') {
 		 			endOfSentence = false;
-		 		} if(temp >= 'a' && temp <= 'z') {
-		 			temp -= 32;
+		 		} if(ch >= 'a' && ch <= 'z') {
+		 			ch -= 32;
 		 			endOfSentence = false;
 		 		}
-		 	} else if(temp >= 'A' && temp <= 'Z' && endOfSentence == false) {
-		 		temp += 32;
+		 	} else if(ch >= 'A' && ch <= 'Z' && endOfSentence == false) {
+		 		ch += 32;
 		 	} 
-		 	newSentence += temp;
+		 	newSentence += ch;
 		 }
 		 	return newSentence;
 		}

cc4d9e8c619a2fba9d17e63163f3b37fcea61850
Noah Clements <noahnoah900@gmail.com>
2018-11-29 00:09:57 -0400

    works as intended

--- ChangeCaseDriver.java
+++ ChangeCaseDriver.java
@@ -3,8 +3,6 @@ public class ChangeCaseDriver {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter a string.");
-
 		String input;
 		//String input2 = input;
 		//String input3 = input;
@@ -15,33 +13,29 @@ public class ChangeCaseDriver {
 	//	System.out.println(p2.toUpperCase());
 	//	System.out.println(p3.toTitleCase());
 		System.out.println("Enter a string ('-1' to quit)");
-		do {
-			input = sc.nextLine();
-			
-			ChangeCase p1 = new ChangeCase(input);
-			System.out.println(p1.toUpperCase());
+		input = sc.nextLine();
+		if(input.equals("-1")) {
 
-			ChangeCase p2 = new ChangeCase(input);
-			System.out.println(p2.toLowerCase());
+			} else {
+				do {
+					ChangeCase p1 = new ChangeCase(input);
+					System.out.println("toUpper: " + p1.toUpperCase());
 
-			ChangeCase p3 = new ChangeCase(input);
-			System.out.println(p3.toggleCase());
+					ChangeCase p2 = new ChangeCase(input);
+					System.out.println("toLower: " + p2.toLowerCase());
 
-			ChangeCase p4 = new ChangeCase(input);
-			System.out.println(p4.toTitleCase());
+					ChangeCase p3 = new ChangeCase(input);
+					System.out.println("toggleCase: " + p3.toggleCase());
 
-			ChangeCase p5 = new ChangeCase(input);
-			System.out.println(p5.toSentenceCase());
+					ChangeCase p4 = new ChangeCase(input);
+					System.out.println("toTitleCase: " + p4.toTitleCase());
 
-			System.out.println("Enter a string ('-1' to quit)");
+					ChangeCase p5 = new ChangeCase(input);
+					System.out.println("toSentenceCase: " + p5.toSentenceCase());
 
-		} while(!input.equals("-1"));
-		
-
-
-
+					System.out.println("\n" + "Enter a string ('-1' to quit)");
+					input = sc.nextLine();
+				} while(!input.equals("-1"));
+			}
 	}
-
-
-
 }
